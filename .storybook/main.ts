a6ca2ec4cdcdef2bd73f4c81f074ffb8e9ec3d96
Rafael Freire <rafaelfreireweb@gmail.com>
2023-08-09 15:06:14 -0300

    viteFinal

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -15,5 +15,12 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/ignite-lab-design-system/'
+    }
+
+    return config
+  }
 };
 export default config;
